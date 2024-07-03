d86885e881ae98b0b3e62e2ee6786e2d899a2e92
leeeunjin <enzn@hufs.ac.kr>
2024-07-03 14:44:40 +0900

    paddle-movement

--- paddlegui.py
+++ paddlegui.py
@@ -22,7 +22,7 @@ class PaddleGUI(Paddle):
         anchor = None
         if side == 'left':
             self.position = [0, HEIGHT//2-35]
-            keys[:] = ['<Up>', '<Down>']  # <-- TODO: change the keys for left side paddle
+            keys[:] = ['<w>', '<s>']  # 변경: 왼쪽 패들을 위한 키 설정
             anchor = NW
 
         if side == "right":
@@ -36,11 +36,15 @@ class PaddleGUI(Paddle):
         self.canvas.bind_all(keys[1], self.move_down)
         self.score_label = self.canvas.create_text(self.position[0], 0, anchor=anchor, text=" Score: 0")
 
-    def move_up(self, event):  # <-- TODO: complete this function by updating the velocity or position appropriately
-        pass
+    def move_up(self, event):
+        self.velocity[1] = -5  # 위로 이동하도록 속도를 설정
+        self.position[1] += self.velocity[1]
+        self.canvas.moveto(self.gui_obj, self.position[0], self.position[1])
 
-    def move_down(self, event):  # <-- TODO: complete this function by updating the velocity or position appropriately
-        pass
+    def move_down(self, event):
+        self.velocity[1] = 5  # 아래로 이동하도록 속도를 설정
+        self.position[1] += self.velocity[1]
+        self.canvas.moveto(self.gui_obj, self.position[0], self.position[1])
 
     def update(self):
         super().update()
